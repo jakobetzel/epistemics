fda0fb78b774e1a7ba49e55dd6f9240d7a695df4
Jakob Etzel <je@25th-floor.com>
2017-11-09 10:44:15 +0100

    added contract data and dynamic loading of the data in the contract page

---
+++ data/contracts/contracts.js
@@ -0,0 +1,19 @@
+var contracts = {
+	1: {
+		file: 'test.pdf',
+		name: 'NDA Mantigma GmbH vs. Etzel, Jakob',
+		entities: [
+			{
+				country: 'AT',
+				type: 'le',
+				id: 1,
+			},
+			{
+				country: 'AT',
+				type: 'np',
+				id: 4
+			}
+		]
+	}
+};
+
---
+++ data/data_munging.js
@@ -0,0 +1,14 @@
+var entities = {
+	AT: {
+		le: AT_legal_entities,
+		np: AT_natural_persons,
+	},
+	DE: {
+		le: DE_legal_entities,
+		np: DE_natural_persons,
+	},
+	UA: {
+		le: UA_legal_entities,
+		np: UA_natural_persons,
+	}
+};
